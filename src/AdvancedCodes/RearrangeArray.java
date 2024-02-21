0245e89b7ecb0ee6dd9f3ba7acabcf5d52c80812
Dheeraj Singh <dhrax21bc@gmail.com>
2024-02-21 18:54:43 +0530

    rearrange_array_code_

---
+++ src/AdvancedCodes/RearrangeArray.java
@@ -0,0 +1,35 @@
+package AdvancedCodes;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class RearrangeArray {
+
+        public static void rearrange(long arr[], int n){
+
+            int max=10000001;
+
+            int left=0,right=n-1;
+
+            for(int i=0; i<n; i++){
+                if(i%2==0){
+                    arr[i]=((arr[right]%max) * max) + arr[i];
+                    right--;
+                }else{
+                    arr[i]=((arr[left]%max) * max) + arr[i];
+                    left++;
+                }
+            }
+
+            for(int i=0; i<n; i++){
+                arr[i]=arr[i]/max;
+            }
+            return;
+        }
+
+    public static void main(String[] args) {
+            long[] arr={1,2,3,4,5,6};
+            rearrange(arr,arr.length);
+            System.out.println(Arrays.toString(arr));
+    }
+    }
--- src/HeapsAndPriorityQueue/Main.java
+++ src/HeapsAndPriorityQueue/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Heaps heap=new Heaps();
-        int[] arr={22,13,17,11,10,14,12};
+        int[] arr={22,13,17,11,10};
         heap.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
