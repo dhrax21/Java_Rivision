b0aae5269053f33db54b8177a7a90f9d5ba481c6
Dheeraj Singh <dhrax21bc@gmail.com>
2024-02-25 16:37:31 +0530

    ReverseWordsInGivenString_code

---
+++ src/AdvancedCodes/ReverseWordsInGivenString.java
@@ -0,0 +1,32 @@
+package AdvancedCodes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseWordsInGivenString {
+    static String reverseWords(String S)
+    {
+        List<Integer> list=new ArrayList<>(); //storing the '.' indices;
+        list.add(0);
+        for(int i=0; i<S.length(); i++){
+            if(S.charAt(i)=='.'){
+                list.add(i);
+            }
+        }
+        list.add(S.length());
+        StringBuilder ans=new StringBuilder();
+
+        for(int i=list.size()-2; i>0; i--){
+            ans.append(S.substring(list.get(i)+1,list.get(i+1)));
+            ans.append(".");
+        }
+        ans.append(S.substring(list.get(0),list.get(1)));
+
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "i.like.this.program.very.much";
+        System.out.println(reverseWords(str));
+    }
+}
--- src/CodeBeginners/MergeSort.java
+++ src/CodeBeginners/MergeSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args) {
-        int[] arr={5,4,9,9,2,4,1};
+        int[] arr={5,4,3,2,1,9};
 
         mergeSort(arr,0,arr.length);
         System.out.println(Arrays.toString(arr));
