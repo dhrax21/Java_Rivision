1b2749279dcfe970b50a58f0cdd22cdfcc3536a7
Dheeraj Singh <dhrax21bc@gmail.com>
2024-03-07 07:51:46 +0530

    additional_code

--- src/GfgPracticeProblem/MinimumJumps.java
+++ src/GfgPracticeProblem/MinimumJumps.java
@@ -3,9 +3,11 @@ package GfgPracticeProblem;
 public class MinimumJumps {
     public static void main(String[] args) {
         int[] arr={1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
-        int N= args.length;
+        int N= arr.length;
 
         System.out.println(minJumps(arr,N));
+
+//        System.out.println(canReach(arr,N));
     }
     static int minJumps(int[] arr,int N){
         int totalJump=0;
@@ -32,5 +34,20 @@ public class MinimumJumps {
 
         }
         return -1;
+    }
+    static int canReach(int[] A, int N) {
+        // code here
+
+        if(N==0 || N==1) return 1;
+
+        int jumps=0;
+
+        for(int i=0; i<=jumps; i++){
+            jumps=Math.max(jumps,i+A[i]);
+            if(i>=N-1)
+                return 1;
+        }
+        return 0;
+
     }
 }
