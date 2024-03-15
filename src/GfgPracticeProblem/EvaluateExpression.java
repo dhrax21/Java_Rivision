eb40f76d83b1a380c1f7e195136229841aea2757
Dheeraj Singh <dhrax21bc@gmail.com>
2024-03-15 16:16:04 +0530

    expression_evaluation_code

---
+++ src/GfgPracticeProblem/EvaluateExpression.java
@@ -0,0 +1,72 @@
+package GfgPracticeProblem;
+
+import java.util.Stack;
+
+public class EvaluateExpression {
+
+    public static void main(String[] args) {
+        String str="-+7*45+20";
+        System.out.println(evaluatePreFix(str));
+    }
+
+    public static int evaluatePostFix(String S)
+    {
+        Stack<Integer> st=new Stack<>();
+
+        int op1,op2;
+
+        for(char ch : S.toCharArray()){
+            if(Character.isDigit(ch)){
+                st.push(ch-'0');
+            }else{
+                op1=st.pop();
+                op2=st.pop();
+
+                switch(ch){
+                    case '+' : st.push(op1+op2);
+                        break;
+                    case '-' : st.push(op2-op1);
+                        break;
+                    case '/' : st.push(op2/op1);
+                        break;
+                    case '*' : st.push(op1*op2);
+                        break;
+                    default : break;
+                }
+            }
+        }
+        return st.peek();
+    }
+
+    public static int evaluatePreFix(String string)
+    {
+        StringBuilder sb=new StringBuilder(string);
+        sb.reverse();
+        String S=sb.toString();
+        Stack<Integer> st=new Stack<>();
+
+        int op1,op2;
+
+        for(char ch : S.toCharArray()){
+            if(Character.isDigit(ch)){
+                st.push(ch-'0');
+            }else{
+                op1=st.pop();
+                op2=st.pop();
+
+                switch(ch){
+                    case '+' : st.push(op1+op2);
+                        break;
+                    case '-' : st.push(op1-op2);
+                        break;
+                    case '/' : st.push(op1/op2);
+                        break;
+                    case '*' : st.push(op1*op2);
+                        break;
+                    default : break;
+                }
+            }
+        }
+        return st.peek();
+    }
+}
