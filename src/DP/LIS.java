d83ec82ac5b485d6ad5db1236ced67cc0b871451
Dheeraj Singh <dhrax21bc@gmail.com>
2024-06-17 08:48:30 +0530

    graph

--- src/DP/LIS.java
+++ src/DP/LIS.java
@@ -7,26 +7,42 @@ public class LIS {
     public static void main(String[] args) {
         int[] arr={12,22,9,33,21,50,41,60,80,3};
 
-        int[] dp=new int[arr.length+1];
-        arr[0]=1;
+        int n= arr.length;
+//        int[] dp=new int[arr.length+1];
 
+        int maxi=-1;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,1);
 
-        int count=0;
-        for(int i=1; i<dp.length-1; i++){
-            int max=0;
-
-            for(int j=0; j<i; j++){
-                if(arr[j]<arr[i]){
-                    if(dp[j]>max){
-                        max=dp[j];
-                    }
+        for(int i=1; i<n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            dp[i]=max+1;
-            if(dp[i]>count){
-                count=dp[i];
-            }
+            maxi = Math.max(maxi, dp[i]);
         }
-        System.out.println(Arrays.toString(dp));
-    }
+        System.out.println(maxi);
+//        arr[0]=1;
+//
+//
+//        int count=0;
+//        for(int i=1; i<dp.length-1; i++){
+//            int max=0;
+//
+//            for(int j=0; j<i; j++){
+//                if(arr[j]<arr[i]){
+//                    if(dp[j]>max){
+//                        max=dp[j];
+//                    }
+//                }
+//            }
+//            dp[i]=max+1;
+//            if(dp[i]>count){
+//                count=dp[i];
+//            }
+//        }
+//        System.out.println(Arrays.toString(dp));
+//
+ }
 }
---
+++ src/GraphDataStructure/ShortestSourceToDestination.java
@@ -0,0 +1,63 @@
+package GraphDataStructure;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class ShortestSourceToDestination {
+    static class Pair{
+        int i;
+        int j;
+        int step;
+
+        Pair(int i,int j,int s){
+            this.i=i;
+            this.j=j;
+            this.step=s;
+        }
+    }
+    int shortestDistance(int N, int M, int A[][], int X, int Y) {
+
+        if(A[0][0]==0 || A[X][Y]==0)
+            return -1;
+
+
+        Queue<Pair> q=new ArrayDeque<>();
+        q.add(new Pair(0,0,0));
+
+        boolean[][] vis=new boolean[N][M];
+        vis[0][0]=true;
+
+        int[] dx={-1,1,0,0};
+        int[] dy={0,0,-1,1};
+
+        while(!q.isEmpty()){
+            Pair temp=q.poll();
+
+            if(temp.i==X && temp.j==Y){
+                return temp.step;
+            }
+
+            for(int i=0; i<4; i++){
+                int nR=temp.i+dx[i];
+                int nC=temp.j+dy[i];
+
+                while(nR>=0 && nC>=0 && nR<N && nC<M && A[nR][nC] != 0 && !vis[nR][nC]){
+                    q.add(new Pair(nR,nC,temp.step+1));
+                    vis[nR][nC]=true;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+//        N=3, M=4
+//        A=[[1,0,0,0],
+//           [1,1,0,1],
+//           [0,1,1,1]]
+//        X=2, Y=3
+//        Output:
+//        5
+//        Explanation:
+//        The shortest path is as follows:
+//        (0,0)->(1,0)->(1,1)->(2,1)->(2,2)->(2,3).
