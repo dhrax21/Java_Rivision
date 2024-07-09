74dc1052a9b3963f3a810c08840ccc0424c2afad
Dheeraj Singh <dhrax21bc@gmail.com>
2024-07-09 10:19:44 +0530

    kmp

---
+++ src/AdvancedCodes/StringMatching.java
@@ -0,0 +1,30 @@
+package AdvancedCodes;
+
+public class StringMatching {
+    public static void main(String[] args) {
+        String str="geeksforgeeks";
+        String pat="geekk";
+
+        boolean flag=false;
+
+        int n=str.length();
+        int m=pat.length();
+
+        for(int i=0; i<n+1-m; i++){
+            int j=0;
+
+            while(j<m && str.charAt(i+j)==pat.charAt(j)){
+                j++;
+
+                if(j==m){
+                    flag=true;
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+        if(!flag){
+            System.out.println("Not found");
+        }
+    }
+}
