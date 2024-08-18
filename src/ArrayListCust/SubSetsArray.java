e557aa926d2efee74f061e298be9ea787a300f09
Dheeraj Singh <dhrax21bc@gmail.com>
2024-08-18 10:58:41 +0530

    substring

--- src/ArrayListCust/SubSetsArray.java
+++ src/ArrayListCust/SubSetsArray.java
@@ -1,56 +1,66 @@
 package ArrayListCust;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SubSetsArray {
     public static void main(String[] args) {
-//        Scanner sc=new Scanner(System.in);
-//        int N=sc.nextInt();
-//        int[] nums=new int[N];
-//        for(int i=0; i<N; i++){
-//            nums[i]=sc.nextInt();
-//        }
+        int N = 3;
+        int[] nums = {1, 2, 3};
+
 //        subSetBinary(N,nums);
-        String str="123";
-        powerSet(str,0,"");
+////        String str="123";
+////        powerSet(str,0,"");
 
-
-
-
-    }
-    static void powerSet(String str, int index, String curr)
-    {
-
-        // base case
-        if (index == str.length()) {
-            System.out.println(curr);
-            return;
-        }
-        powerSet(str, index + 1, curr + str.charAt(index));
-        powerSet(str, index + 1, curr);
-
+        subStringsPrint("10101", new ArrayList<>());
     }
 
+    static void subStringsPrint(String s, List<String> sets) {
 
-    static void subSetBinary(int N,int[] nums){
-        System.out.println("Printing Subsets--");
-
-        int totalSubsets=(int)Math.pow(2,N);
-
-        for(int i=0; i<totalSubsets; i++){
-            String set="";
-            int temp=i;
-            for(int j=N-1; j>=0; j--){
-                int rem=temp % 2;
-                temp=temp/2;
-                if(rem==0){
-                    set="-\t"+set;
-                }else{
-                    set=nums[j]+"\t"+set;
-                }
+        for (int i = 0; i < s.length(); i++) {
+            String sub = "";
+            for (int j = i; j < s.length(); j++) {
+                sub += s.charAt(j);
+                sets.add(sub);
+                System.out.print(sub + " ");
             }
-            System.out.println(set);
+            System.out.println();
         }
     }
-
 }
+//    static void powerSet(String str, int index, String curr)
+//    {
+//
+//        // base case
+//        if (index == str.length()) {
+//            System.out.println(curr);
+//            return;
+//        }
+//        powerSet(str, index + 1, curr + str.charAt(index));
+//        powerSet(str, index + 1, curr);
+//
+//    }
+
+
+//    static void subSetBinary(int N,int[] nums){
+//        System.out.println("Printing Subsets--");
+//
+//        int totalSubsets=(int)Math.pow(2,N);
+//
+//        for(int i=0; i<totalSubsets; i++){
+//            String set="";
+//            int temp=i;
+//            for(int j=N-1; j>=0; j--){
+//                int rem=temp % 2;
+//                temp=temp/2;
+//                if(rem==0){
+//                    set="-\t"+set;
+//                }else{
+//                    set=nums[j]+"\t"+set;
+//                }
+//            }
+//            System.out.println(set);
+//        }
+//    }
+
