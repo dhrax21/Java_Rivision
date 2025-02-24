edfad79fe11029a934c7a2c45693cf19ba8f2878
Dheeraj Singh <dhrax21bc@gmail.com>
2025-02-24 16:47:50 +0530

    que_stacks

---
+++ src/QueueY/FirstNegativeInWindowK.java
@@ -0,0 +1,31 @@
+package QueueY;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class FirstNegativeInWindowK {
+    static List<Integer> FirstNegativeInteger(int arr[], int k) {
+        int N=arr.length;
+        List<Integer> ans=new ArrayList<>(N-k+1);
+        Queue<Integer> q=new LinkedList<>();
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]<0) q.add(i);
+        }
+
+        for(int i=0; i<N-k+1; i++){
+            if(!q.isEmpty() && q.peek()<i) q.remove();
+
+            if(!q.isEmpty() && q.peek()<i+k-1)
+                ans.add(arr[q.peek()]);
+            else if(q.isEmpty())
+                ans.add(0);
+            else
+                ans.add(0);
+        }
+
+        return ans;
+    }
+}
